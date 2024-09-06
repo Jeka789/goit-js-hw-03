c31a58e003bb7445e1dc94c2b84796b656aa692b
Jeka <asyfufi@gmail.com>
2024-09-06 14:13:11 +0300

    yceee

---
+++ js/task-2.js
@@ -0,0 +1,18 @@
+function makeArray(firstArray, secondArray, maxLength) {
+    const obdnArray = firstArray.concat(secondArray);
+
+    if (obdnArray.length > maxLength) {
+        return obdnArray.slice(0, maxLength);
+    }
+    
+    return obdnArray;
+}
+
+
+
+console.log(makeArray(["Mango", "Poly"], ["Ajax", "Chelsea"], 3)); // ["Mango", "Poly", "Ajax"]
+console.log(makeArray(["Mango", "Poly", "Houston"], ["Ajax", "Chelsea"], 4)); // ["Mango", "Poly", "Houston", "Ajax"]
+console.log(makeArray(["Mango"], ["Ajax", "Chelsea", "Poly", "Houston"], 3)); // ["Mango", "Ajax", "Chelsea"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 2)); // ["Earth", "Jupiter"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 4)); // ["Earth", "Jupiter", "Neptune", "Uranus"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus", "Venus"], 0)); // []
